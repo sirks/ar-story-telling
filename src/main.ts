2f70a16febf3b4a21699c3669b49b18b8db39430
kriss <kriss.pujats@gmail.com>
2018-07-03 18:32:56 +0200

    start

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import App from './App.vue'
+import Start from './views/start/Start.vue'
 import Home from './views/home/Home.vue'
 import Give from './views/give/Give.vue'
 import Negotiate from './views/negotiate/Negotiate.vue'
@@ -16,7 +17,7 @@ const router = new VueRouter({
   mode: 'history',
   base: __dirname,
   routes: [
-    {path: '/', component: Home},
+    {path: '/', component: Start},
     {path: '/home', component: Home},
     {path: '/give', component: Give},
     {path: '/negotiate', component: Negotiate},
---
+++ src/views/start/Start.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class='start'>
+    <router-link class='button' :to="'home'">START</router-link>
+  </div>
+</template>
+
+<script lang='ts'>
+  import {Component, Vue} from 'vue-property-decorator';
+
+  @Component
+  export default class Start extends Vue {
+
+  }
+</script>
+
+<style scoped>
+
+  .start {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+  }
+
+  .button {
+    padding: 0 5% 1%;
+    font-size: 3.5vw;
+    font-weight: bold;
+    color: inherit;
+    text-decoration: none;
+    border-bottom: 0.3vw solid #555;
+  }
+
+</style>
