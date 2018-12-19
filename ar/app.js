ae606b30f8efd0f799bb718633122849685dedcf
kriss <kriss.pujats@gmail.com>
2018-12-19 13:47:38 +0200

    check href exists

--- ar/app.js
+++ ar/app.js
@@ -3,6 +3,8 @@ import aframe from 'aframe';
 window.onload = () => {
   for (let marker of document.getElementsByTagName('a-marker')){
     const href = marker.getAttribute('href');
-    marker.addEventListener('markerFound', () => window.location.href = href)
+    if (href) {
+      marker.addEventListener('markerFound', () => window.location.href = href)
+    }
   }
 };
